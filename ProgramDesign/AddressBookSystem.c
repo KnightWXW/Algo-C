024aa72b92f1ba7a6b37519f18b92772dce48d5e
KnightWXW <knight960903@gmail.com>
2025-02-27 00:32:41 +0800

    A;go-C

--- ProgramDesign/AddressBookSystem.c
+++ ProgramDesign/AddressBookSystem.c
@@ -6,7 +6,7 @@
 //      void Add(string name, string number): 添加联系人：
 //          姓名为 name, 电话号码为 number, 若已存在则不作处理:
 //      string Update(string name, string number): 更新联系人：
-//          若已存在联系人name, 则将此联系人的电话号码改为 number；
+//          若已存在联系人name, 则将此联系人的电话号码改为 number, 返回之前的number;
 //          若不存在联系人name, 则不做处理并返回 “No Such Person”；
 //      string Delete(string name): 删除联系人：
 //          若已存在联系人name, 则删除此联系人记录，并返回其电话号码；
@@ -27,28 +27,64 @@ typedef struct
 
 AddressBookSystem *AddressBookSystemCreate()
 {
+    AddressBookSystem *obj = NULL;
+    return obj;
 }
 
-void AddressBookSystemAdd(AddressBookSystem *obj, char *name, char *number)
+void AddressBookSystemAdd(AddressBookSystem **obj, char *name, char *number)
 {
-
+    AddressBookSystem *cur;
+    HASH_FIND_STR(*obj, name, cur);
+    if (cur == NULL)
+    {
+        cur = (AddressBookSystem *)malloc(sizeof(AddressBookSystem));
+        memset(cur->name, 0, sizeof(cur->name));
+        memset(cur->number, 0, sizeof(cur->number));
+        strcpy(cur->name, name);
+        strcpy(cur->number, number);
+        HASH_ADD_STR(*obj, name, cur);
+    }
     return;
 }
 
-char *AddressBookSystemUpdate(char *name, char *number)
+char *AddressBookSystemUpdate(AddressBookSystem *obj, char *name, char *number)
 {
+    AddressBookSystem *cur;
+    HASH_FIND_STR(*obj, name, cur);
+    if (cur == NULL)
+    {
+        return "No Such Person";
+    }
+    char* ans = (char*)malloc(sizeof(char)*sizeof(cur->number));
+    strcpy(ans, cur->number);
+    strcpy(cur->number, number);
+    return ans;
 }
 
-char *AddressBookSystemDelete(char *name)
+char *AddressBookSystemDelete(AddressBookSystem *obj, char *name)
 {
+    AddressBookSystem *cur;
+    HASH_FIND_STR(*obj, name, cur);
+    if (cur == NULL)
+    {
+        return "No Such Person";
+    }
+    char* ans = (char*)malloc(sizeof(char)*sizeof(cur->number));
+    strcpy(ans, cur->number);
+    HASH_DEL(cur, name);
+    return ans;
 }
 
-char **AddressBookSystemPrefer(char *s)
+char **AddressBookSystemPrefer(AddressBookSystem *obj, char *s)
 {
+    
 }
 
-void AddressBookSystemFree()
+void AddressBookSystemFree(AddressBookSystem **obj)
 {
+    HASH_CLEAR(hh, *obj);
+    free(obj);
+    return;
 }
 
 int main()
@@ -56,27 +92,27 @@ int main()
     AddressBookSystem *addressBookSystem = AddressBookSystemCreate();
     AddressBookSystemAdd(addressBookSystem, "Herry", "123");
     AddressBookSystemAdd(addressBookSystem, "Merry", "456");
-    char* a1 = AddressBookSystemUpdate(addressBookSystem,"Merry", "789"); // 456
+    char *a1 = AddressBookSystemUpdate(addressBookSystem, "Merry", "789"); // 456
     printf("更新结果为:\n");
     PrintString(a1);
     FreeString(a1);
     int l2 = 0;
-    char** a2 = AddressBookSystemPrefer(addressBookSystem,"Me", &l2); // {"Merry", "789"}
+    char **a2 = AddressBookSystemPrefer(addressBookSystem, "Me", &l2); // {"Merry", "789"}
     printf("查询结果为:\n");
     PrintStringVec(a2, l2);
     FreeStringVec(a2, l2);
-    char* a3 = AddressBookSystemDelete(addressBookSystem,"Merry"); // 789
+    char *a3 = AddressBookSystemDelete(addressBookSystem, "Merry"); // 789
     printf("删除结果为:\n");
     PrintString(a3);
     FreeString(a3);
-    AddressBookSystemAdd(addressBookSystem,"Herry", "234");
-    AddressBookSystemAdd(addressBookSystem,"Henry", "2564");
-    char* a4 = AddressBookSystemUpdate(addressBookSystem,"Merry", "111"); // No Such Person
+    AddressBookSystemAdd(addressBookSystem, "Herry", "234");
+    AddressBookSystemAdd(addressBookSystem, "Henry", "2564");
+    char *a4 = AddressBookSystemUpdate(addressBookSystem, "Merry", "111"); // No Such Person
     printf("更新结果为:\n");
     printString(a4);
     FreeString(a4);
     int l5 = 0;
-    char** a5 = AddressBookSystemPrefer(addressBookSystem,"He", &l5); // {{"Henry", "2564"},{"Herry", "234"}}
+    char **a5 = AddressBookSystemPrefer(addressBookSystem, "He", &l5); // {{"Henry", "2564"},{"Herry", "234"}}
     printf("查询结果为:\n");
     PrintStringVec(a5, l5);
     FreeStringVec(a5, l5);
