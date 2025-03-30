3a40172ca51d760012a823067c1f1d7cda75f608
KnightWXW <knight960903@gmail.com>
2025-03-30 13:32:03 +0800

    A;go-C

---
+++ GreedyAlgorithm/DistributeConferenceTickets.c
@@ -0,0 +1,61 @@
+#include "../Mybasic/mybasic.h"
+
+//      Huawei: 分发大会门票
+
+//      HC大会门票数量有限，组织者在前台分发门票，
+//      并把每轮分发门票的时间点按时间先后顺序记录在数组distribute。
+//      每轮有num张门票供分发，具体分发规则如下：
+//          每位同事在分发时间点（含分发时间）之前到达才能领票
+//          先到先得，一人一票，本轮余票作废
+//          本轮没有拿到票的会等到下一轮派发，直到拿到票或者所有门票分发完毕
+//      每位同事预计到达时间按时间先后顺序记录在数组arrive中，
+//      假设你也想去参加大会，请问什么时候去保证可以拿到票？
+//      示例1：
+//          输入：distribute = {11, 20}
+//              num = 2
+//              arrive = {11,12,15,15,15}
+//          输出：14
+//      示例2：
+//          输入：distribute = {13, 20}
+//              num = 3
+//              arrive = {11,11,11,12,16,17,18}
+//          输出：15
+//      提示：1 <= distriute.size() <= 10^3 distribute升序
+//          1 <= distriute[i] <= 10^5
+//          1 <= num <= 100
+//          1 < arrive.size() <= 10^5 arrive升序
+//          1 <= arrive[i] <= 10^5
+
+int DistributeConferenceTickets(int *distribute, int distributeSize, int num, int *arrive, int arriveSize);
+
+int CompareInt(const int *a, const int *b)
+{
+    return *(int *)a - *(int *)b;
+}
+
+int main()
+{
+    int n1 = generateRandomNum(1, 10);
+    int n2 = generateRandomNum(1, 20);
+    int num = generateRandomNum(1, 5);
+    int *distribute = GenerateRandomVec(1, 30, n1);
+    qsort(distribute, n1, sizeof(int), CompareInt);
+    printf("distribute 数组 元素为: ");
+    PrintVecElement(distribute, n1);
+    int *arrive = GenerateRandomVec(1, 30, n2);
+    qsort(arrive, n2, sizeof(int), CompareInt);
+    printf("arrive 数组 元素为: ");
+    PrintVecElement(arrive, n2);
+    printf("每轮有 %d 张门票供分发\n", num);
+    int time = DistributeConferenceTickets(distribute, n1, num, arrive, n2);
+    printf("%d 时 去保证可以拿到票。\n", time);
+    FreeVec(distribute);
+    FreeVec(arrive);
+}
+
+// 贪心算法：
+// Time: O(N)
+// Space: O(1)
+int DistributeConferenceTickets(int *distribute, int distributeSize, int num, int *arrive, int arriveSize)
+{
+}
---
+++ ProgramDesign/CodeStateSystem.c
@@ -0,0 +1,92 @@
+
+
+//      Huawei: 编程语言设计系统
+
+//      设计一个编程语言设计系统：
+//      CodeStateSystem(products[]): 初始化产品及其代码仓关系，所有代码仓的代码量初始化为0
+//          products[i] = [productId, repolds[]]: 表示一个产品及其代码仓列表。
+//          产品，代码仓均是全局唯一的，一个代码仓仅归属于某一产品。
+//      ChangeCodeLines(int repolds, int languageId, int codeline):
+//          代码仓repold中某种语言languageId的代码行的变化量为codeline
+//          (正值表示增加,负值表示减少)。
+//          最后返回此代码仓中该语言的代码总行数。
+//          用例保证代码仓已存在，一个代码仓中某语言的代码行数不会减少为负值。
+//      StatLanguage(int productId): 统计产品productId所用到的各语言的代码总行数，
+//          并按要求返回语言Id列表，productId为0表示所有产品，非0表示指定产品。
+//          返回要求：只返回代码量大于0的语言，优先按照语言的代码量降序，
+//          若代码量相同，则再按照语言id升序
+
+typedef struct
+{
+    int productId;
+    int *repolds;
+    int repoldsSize;
+} Product;
+
+typedef struct
+{
+
+} CodeStateSystem;
+
+CodeStateSystem *CodeStateSystemCreate(Product *products, int ProductSize)
+{
+}
+
+int CodeStateSystemChangeCodeLines(CodeStateSystem *obj, int repolds, int languageId, int codeline)
+{
+}
+
+int *CodeStateSystemStatLanguage(CodeStateSystem *obj, int productId, int *returnSize)
+{
+}
+
+void CodeStateSystemFree(CodeStateSystem *obj)
+{
+}
+
+int main()
+{
+    int productSize = 3;
+    Product *p = (Product *)malloc(sizeof(Product) * ProductSize);
+    p[0]->productId = 10;
+    p[0]->repoldsSize = 2;
+    p[0]->repolds = (int *)malloc(sizeof(int) * p[0]->repoldsSize);
+    p[0]->repolds[0] = 102;
+    p[0]->repolds[1] = 101;
+    p[1]->productId = 12;
+    p[1]->repoldsSize = 1;
+    p[1]->repolds = (int *)malloc(sizeof(int) * p[1]->repoldsSize);
+    p[1]->repolds[0] = 122;
+    p[2]->productId = 22;
+    p[2]->repoldsSize = 1;
+    p[2]->repolds = (int *)malloc(sizeof(int) * p[2]->repoldsSize);
+    p[2]->repolds[0] = 221;
+    CodeStateSystem *obj = CodeStateSystemCreate(p, productSize);
+    int l1 = 3;
+    int c1 = CodeStateSystemChangeCodeLines(obj, 221, l1, 2500); // 2500
+    printf("代码仓中语言 %d 的代码总行数为 %d\n", l1, c1);
+    int l2 = 2;
+    int c2 = CodeStateSystemChangeCodeLines(obj, 102, l2, 2100); // 2100
+    printf("代码仓中语言 %d 的代码总行数为 %d\n", l2, c2);
+    int l3 = 1;
+    int c3 = CodeStateSystemChangeCodeLines(obj, 101, l3, 2000); // 2000
+    printf("代码仓中语言 %d 的代码总行数为 %d\n", l3, c3);
+    int l4 = 10;
+    int sr1 = 0;
+    int *s1 = CodeStateSystemStatLanguage(obj, l4, &sr1); // {2, 1}
+    printf("要求返回语言 %d 列表为 \n", l4);
+    PrintVecElement(s1, sr1);
+    int l5 = 22;
+    int sr2 = 0;
+    int *s2 = CodeStateSystemStatLanguage(obj, l5, &sr2); // {3}
+    printf("要求返回语言 %d 列表为 \n", l5);
+    PrintVecElement(s2, sr2);
+    int c4 = CodeStateSystemChangeCodeLines(obj, 102, 2, -100); // 2000
+    printf("代码仓中语言 %d 的代码总行数为 %d\n", l1, c1);
+    int l6 = 0;
+    int sr3 = 0;
+    int *s3 = CodeStateSystemStatLanguage(obj, l6, &sr3); // {3, 1, 2}
+    printf("要求返回语言 %d 列表为 \n", l6);
+    PrintVecElement(s3, sr3);
+    CodeStateSystemFree(obj);
+}
