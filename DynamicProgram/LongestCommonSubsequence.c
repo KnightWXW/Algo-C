2d459c1572b42e3928ad5996d7acf090b92e62fc
KnightWXW <knight960903@gmail.com>
2025-06-18 23:45:19 +0800

    Algo-C

---
+++ DynamicProgram/LongestCommonSubsequence.c
@@ -0,0 +1,48 @@
+#include "../Mybasic/mybasic.h"
+
+//      LeetCode 1143. 最长公共子序列
+
+//      链接：https://leetcode.cn/problems/longest-common-subsequence/
+
+//      给定两个字符串 text1 和 text2，返回这两个字符串的最长 公共子序列 的长度。
+//      如果不存在 公共子序列 ，返回 0 。
+//      一个字符串的 子序列 是指这样一个新的字符串：
+//      它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+//      例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。
+//      两个字符串的 公共子序列 是这两个字符串所共同拥有的子序列。
+//      示例 1：
+//          输入：text1 = "abcde", text2 = "ace"
+//          输出：3
+//          解释：最长公共子序列是 "ace" ，它的长度为 3 。
+//      示例 2：
+//          输入：text1 = "abc", text2 = "abc"
+//          输出：3
+//          解释：最长公共子序列是 "abc" ，它的长度为 3 。
+//      示例 3：
+//          输入：text1 = "abc", text2 = "def"
+//          输出：0
+//          解释：两个字符串没有公共子序列，返回 0
+//      提示：
+//          1 <= text1.length, text2.length <= 1000
+//          text1 和 text2 仅由小写英文字符组成。
+
+int LongestCommonSubsequence_A(char *s);
+int DfsLongestCommonSubsequence_A(char *s, int start, int end);
+int LongestCommonSubsequence_B(char *s);
+int DfsLongestCommonSubsequence_B(char *s, int start, int end);
+int LongestCommonSubsequence_C(char *s);
+
+int main()
+{
+    int n = GenerateRandomNum(0, 20);
+    char arr[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g'};
+    int l = strlen(arr);
+    char *s = GenerateRandomString(n, arr, l);
+    PrintString(s);
+    int ansA = LongestCommonSubsequence_A(s);
+    int ansB = LongestCommonSubsequence_B(s);
+    int ansC = LongestCommonSubsequence_C(s);
+    printf("(暴力递归) 字符串 中 最长公共子序列 的 长度 为 %d\n", ansA);
+    printf("(记忆化搜索) 字符串 中 最长公共子序列 的 长度 为 %d\n", ansB);
+    printf("(动态规划) 字符串 中 最长公共子序列 的 长度 为 %d\n", ansC);
+}
---
+++ DynamicProgram/RobotWalking.c
@@ -0,0 +1,63 @@
+#include "../Mybasic/mybasic.h"
+
+//      假设有排成一行的 n 个位置，记为 1~n ,n>=2
+//      开始时机器人在其中的 cur 位置上(M一定是 1~N 中的一个)
+//      如果机器人来到 1 位置，那么下一步只能往右来到 2 位置;
+//      如果机器人来到 n 位置，那么下一步只能往左来到 n-1 位置;
+//      如果机器人来到中间位置，那么下一步可以往左走或者往右走;
+//      规定机器人必须走 cnt 步，最终能来到 end 位置(P 也是 1-n 中的一个)的方法有多少种?
+//      给定四个参数 n、cur、cnt、end，返回方法数.
+
+int RobotWalking_A(int n, int cur, int cnt, int end);
+int RobotWalking_B(int n, int cur, int cnt, int end);
+int DfsRobotWalking_B(int n, int cur, int cnt, int end, int **arr);
+int RobotWalking_C(int n, int cur, int cnt, int end);
+int RobotWalking_D(int n, int cur, int cnt, int end);
+
+int main()
+{
+    int n = 5;
+    int cur = 2;
+    int cnt = 6;
+    int end = 4;
+    int ansA = RobotWalking_A(n, cur, cnt, end);
+    int ansB = RobotWalking_B(n, cur, cnt, end);
+    int ansC = RobotWalking_C(n, cur, cnt, end);
+    int ansD = RobotWalking_C(n, cur, cnt, end);
+    printf("暴力递归：%d\n", ansA);             // 13
+    printf("记忆化搜索：%d\n", ansB);           // 13
+    printf("动态规划：%d\n", ansC);             // 13
+    printf("动态规划（空间优化）：%d\n", ansD); // 13
+}
+
+// 暴力递归：
+// Time: O(2^N)
+// Space: O(N)
+int RobotWalking_A(int n, int cur, int cnt, int end)
+{
+}
+
+// 记忆化搜索：
+// Time: O(2^N)
+// Space: O(N)
+int RobotWalking_B(int n, int cur, int cnt, int end)
+{
+}
+
+int DfsRobotWalking_B(int n, int cur, int cnt, int end, int **arr)
+{
+}
+
+// 动态规划：
+// Time: O(cnt * n)
+// Space: O(cnt * n)
+int RobotWalking_C(int n, int cur, int cnt, int end)
+{
+}
+
+// 动态规划(空间优化)：
+// Time: O(n)
+// Space: O(n)
+int RobotWalking_D(int n, int cur, int cnt, int end)
+{
+}
