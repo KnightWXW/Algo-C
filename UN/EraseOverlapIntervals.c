b821177b0ef72d9fb09527074f10c4cc1b75bae2
Knight <knight960903@google.com>
2026-08-28 19:53:48 +0800

    Algo-C

--- UN/EraseOverlapIntervals.c
+++ UN/EraseOverlapIntervals.c
@@ -12,11 +12,11 @@
 //          输出: 1
 //          解释: 移除 [1,3] 后，剩下的区间没有重叠。
 //      示例 2:
-//          输入: intervals = [ [1,2], [1,2], [1,2] ]
+//          输入: intervals = [[1,2], [1,2], [1,2]]
 //          输出: 2
 //          解释: 你需要移除两个 [1,2] 来使剩下的区间没有重叠。
 //      示例 3:
-//          输入: intervals = [ [1,2], [2,3] ]
+//          输入: intervals = [[1,2], [2,3]]
 //          输出: 0
 //          解释: 你不需要移除任何区间，因为它们已经是无重叠的了。
 //      提示:
@@ -26,3 +26,30 @@
 
 int EraseOverlapIntervals(int** intervals, int intervalsSize, int* intervalsColSize);
 
+int main()
+{
+    int r1 = 4;
+    int c1 = 2;
+    int** vec1 = (int**)malloc(sizeof(int*) * r1);
+    for(int i = 0; i < r1; i++)
+    {
+        vec1[i] = (int*)malloc(sizeof(int) * c1);
+    }
+    vec1[0][0] = 1;
+    vec1[0][1] = 2;
+    vec1[1][0] = 2;
+    vec1[1][1] = 3;
+    vec1[2][0] = 3;
+    vec1[2][1] = 4;
+    vec1[3][0] = 1;
+    vec1[3][1] = 3;
+    PrintVecElement2D(vec1, r1, c1);
+    int ans1 = EraseOverlapIntervals((int **)vec1, r1, &c1);
+    printf("需要移除区间的最小数量为:%d\n", ans1);
+    FreeVec2D(vec1, r1);
+}
+
+int EraseOverlapIntervals(int** intervals, int intervalsSize, int* intervalsColSize)
+{
+    
+}
